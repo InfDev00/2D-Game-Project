f71a6c8b5b67331715a57f6b728232c270bd14f2
InfDev00 <119485240+InfDev00@users.noreply.github.com>
2023-07-26 20:13:11 +0900

    에셋 추가

---
+++ Assets/Scripts/Player Manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject player;
+    private GameObject idle0;
+    private GameObject idel1;
+    private GameObject
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
